8c47bf2180e3162d39611201f3e8d3c892255fdc
lngdao <longdao.work@gmail.com>
2026-04-05 22:54:54 +0700

    release: 2026.405.1

--- wxt.config.ts
+++ wxt.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   }),
   manifest: {
     name: 'Social-DL',
-    version: '2026.403.1',
+    version: '2026.405.1',
     permissions: ['webRequest', 'storage', 'sidePanel', 'downloads', 'offscreen'],
     host_permissions: [
       '*://*.facebook.com/*',
